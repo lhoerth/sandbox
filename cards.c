6c30964388e9ec0a91ce866d1872a2cd73c669cf
lhoerth <lhoerth@mail.greenriver.edu>
2014-10-05 01:22:09 -0700

    Program 02: Version 2

--- cards.c
+++ cards.c
@@ -1,9 +1,9 @@
 /*
 ** Logan Hoerth - lhoerth@mail.greenriver.edu
 ** 10-4-14
-** Program 02: cards.c 
+** Program 02: cards.c (Version 2)
 ** 
-** time spent: 
+** time spent: 10 minutes
 */
 
 #include <stdio.h>
@@ -12,22 +12,35 @@
 int main(void)
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
-	int val = 0;
-
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;
-	} else {
-		val = atoi(card_name);
+	int count = 0;
+	while (card_name[0] != 'X'){
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+		int val = 0;
+		switch (card_name[0]) {
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			continue;
+		default:
+			val = atoi(card_name);
+			if ((val < 1) || (val > 10)) {
+				puts("Oops. You broke it.");
+				continue;
+			}
+		}
+		if ((val > 2) && (val < 7)) {
+			count++;
+		} else if (val == 10) {
+			count--;
+		}
+		printf("Current count: %i\n", count);
 	}
-	printf("The card value is: %i\n", val);
-
 	return 0;
 }
