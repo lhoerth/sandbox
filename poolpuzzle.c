871cf5a0533bb136f209ea14fc2abcb7e75f641e
lhoerth <lhoerth@mail.greenriver.edu>
2014-12-08 02:58:03 -0800

    Poolpuzzle v2

--- poolpuzzle.c
+++ poolpuzzle.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
-void catalog(const char *name, const char *species, int teeth, int age)
+struct fish {
+	const char *name;
+	const char *species;
+	int teeth;
+	int age;
+};
+
+void catalog(struct fish f)
 {
 	printf("%s is a %s with %i teeth. He is %i\n",
-		name, species, teeth, age);
+		f.name, f.species, f.teeth, f.age);
 }
 
-void label(const char *name, const char *species, int teeth, int age)
+void label(struct fish f)
 {
 	printf("Name: %s\nSpecies: %s\n%i years old, %i teeth\n",
-		name, species, teeth, age );
+		f.name, f.species, f.age, f.teeth );
 }
 
-int main()
+int main(void)
 {
-	catalog("Snappy", "Piranha", 69, 4);
-	label("Snappy", "Piranha", 69, 4);
+	struct fish snappy = {"Snappy", "Piranha", 69, 4};
+	catalog(snappy);
+	label(snappy);
 	return 0;
 }
